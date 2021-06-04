3d240cff9a76bf310da796e95d5ebe589c246d23
poldeeek <pawelp258@gmail.com>
2021-06-04 12:20:23 +0200

    cleaning up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import './style.css';
-
-import img from './image.PNG';
-
 const App: React.FC = () => {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      <img src={img} width="300px" height="300px" alt="sth" />
-    </div>
-  );
+  return <div className="container"></div>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,12 @@
+import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 
-ReactDom.render(<App />, document.getElementById('root'));
+import './style.css';
+
+ReactDom.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
