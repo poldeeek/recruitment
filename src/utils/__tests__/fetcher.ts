b15772d6e32be0ec55cfeb9b3665a3f14c07fc88
poldeeek <pawelp258@gmail.com>
2021-06-04 21:30:12 +0200

    Basic tests for fetcher file

---
+++ src/utils/__tests__/fetcher.ts
@@ -0,0 +1,109 @@
+import _ from 'lodash';
+import { fetcher, readData } from '../fetcher';
+
+describe('Fetcher func', () => {
+  it('Basic', async () => {
+    const mockSuccessResponse = data;
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise,
+    });
+    const globalRef = global;
+
+    globalRef.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+
+    const response = await fetcher('https://some-url.pl');
+
+    expect(
+      _.isEqual(JSON.stringify(response), JSON.stringify(readData(data)))
+    ).toBe(true);
+  });
+});
+
+describe('Read data func', () => {
+  const expectedResult = [
+    {
+      course: 'Course1',
+      openedLessonsCount: 0,
+      projects: [{ project: 'Project1', completedLessonsCount: 0 }],
+    },
+    {
+      course: 'Course2',
+      openedLessonsCount: 2,
+      projects: [
+        { project: 'Project2', completedLessonsCount: 1 },
+        { project: 'Project3', completedLessonsCount: 1 },
+      ],
+    },
+  ];
+
+  it('Basic', () => {
+    const result = readData(data);
+    expect(_.isEqual(result, expectedResult)).toBe(true);
+  });
+
+  it('Empty array', () => {
+    const result = readData([]);
+    expect(_.isEqual(result, [])).toBe(true);
+  });
+});
+
+const data = [
+  {
+    project: 'Project1',
+    course: 'Course1',
+    person: 'Paweł',
+    email: 'email@wp.pl',
+    department: '',
+    location: '',
+    courseStartedDate: '',
+    openedLessonsCount: '0',
+    completedDate: '',
+    completedLessonsCount: 0,
+    totalLessonsCount: 1,
+    haveNotStarted: '1',
+    notOnSchedule: '1',
+    haveStarted: '0',
+    quizScore: 0,
+    quizScoreTotal: 1,
+    certificateTitle: null,
+  },
+  {
+    project: 'Project2',
+    course: 'Course2',
+    person: 'Paweł2',
+    email: 'email2@wp.pl',
+    department: '',
+    location: '',
+    courseStartedDate: '2021-03-09 10:02:35',
+    openedLessonsCount: '1',
+    completedDate: '2021-03-09 10:05:03',
+    completedLessonsCount: 1,
+    totalLessonsCount: 1,
+    haveNotStarted: '0',
+    notOnSchedule: '0',
+    haveStarted: '1',
+    quizScore: 0,
+    quizScoreTotal: 0,
+    certificateTitle: null,
+  },
+  {
+    project: 'Project3',
+    course: 'Course2',
+    person: 'Paweł3',
+    email: 'email3@wp.pl',
+    department: '',
+    location: '',
+    courseStartedDate: '2021-03-09 10:02:35',
+    openedLessonsCount: '1',
+    completedDate: '2021-03-09 10:05:03',
+    completedLessonsCount: 1,
+    totalLessonsCount: 1,
+    haveNotStarted: '0',
+    notOnSchedule: '0',
+    haveStarted: '1',
+    quizScore: 0,
+    quizScoreTotal: 0,
+    certificateTitle: null,
+  },
+];
