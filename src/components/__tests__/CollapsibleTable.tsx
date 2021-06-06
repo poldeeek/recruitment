c9a515d2104bdde4d26a9936cf670d48b109260f
poldeeek <pawelp258@gmail.com>
2021-06-06 02:02:32 +0200

    CollapsibleTable test

---
+++ src/components/__tests__/CollapsibleTable.tsx
@@ -0,0 +1,32 @@
+import '@testing-library/jest-dom/extend-expect';
+import CollapsibleTable from '../CollapsibleTable';
+import { render } from '@testing-library/react';
+import { ICourse } from '../../types';
+
+jest.mock('../Row.tsx', () => {
+  const Row: React.FC<ICourse> = () => <tr></tr>;
+  return Row;
+});
+
+const props = [
+  {
+    course: 'Course1',
+    openedLessonsCount: 8,
+    projects: [],
+  },
+  {
+    course: 'Course2',
+    openedLessonsCount: 1,
+    projects: [],
+  },
+];
+
+test('Basic render', () => {
+  const component = render(<CollapsibleTable courses={props} />);
+  expect(component.baseElement).toMatchSnapshot();
+});
+
+test('Basic render, no courses', () => {
+  const component = render(<CollapsibleTable courses={[]} />);
+  expect(component.baseElement).toMatchSnapshot();
+});
